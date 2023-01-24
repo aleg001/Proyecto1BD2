97c31cb36f11ef8b877eb0ff1404016387a2f4f2
Ale Gomez <gom20347@uvg.edu.gt>
2023-01-23 20:48:03 -0600

    added config por project

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/mevn.jpg" width="350" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  components: {},
+};
+</script>
