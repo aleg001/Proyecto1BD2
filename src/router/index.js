676bcba213a8d11b53425d7b1908ff1d5ed43746
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-05 18:35:53 -0600

    added new route

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,21 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/MainPage",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "MainPage",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/MainPage.vue"),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
