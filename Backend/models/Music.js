24c011c0be4af07f996ba03e4b29acde1e31ac1e
yong park <yong.park123456@gmail.com>
2023-02-05 21:40:03 -0600

    se muestra la colection de usuarios y se esta intentando mostrar el de music

---
+++ Backend/models/Music.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const MusicSchema = new mongoose.Schema({
+    Username:{type:String, required:true, trim:true},
+    Nombre:{type:String, required:true, trim:true},
+    Apellido:{type:String, required:true, trim:true},
+    Email:{type:String, required:true, trim:true},
+    password:{type:String, required:true, trim:true},
+    Playlist:{type:Object, required:true, trim:true},
+})
+
+const MusicModel = mongoose.model("music",MusicSchema)
+
+export default MusicModel
--- Backend/routes/web.js
+++ Backend/routes/web.js
@@ -1,8 +1,10 @@
 import express from "express";
 import UsuarioController from "../controllers/usuariosController.js";
+import MusicController from "../controllers/musicController.js";
 
 const router = express.Router();
 
 router.get("/usuarios", UsuarioController.getAllDocc);
+router.get("/music", MusicController.getAllDoc);
 
 export default router;
