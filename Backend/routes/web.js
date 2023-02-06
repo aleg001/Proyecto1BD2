83e49e22adfbafd53d62d4d29b8b92d8e068650b
yong park <yong.park123456@gmail.com>
2023-02-06 01:01:38 -0600

    Se demuestra el collections de musics

--- Backend/routes/web.js
+++ Backend/routes/web.js
@@ -4,7 +4,7 @@ import MusicController from "../controllers/musicController.js";
 
 const router = express.Router();
 
-router.get("/usuarios", UsuarioController.getAllDocc);
 router.get("/music", MusicController.getAllDoc);
+router.get("/usuarios", UsuarioController.getAllDocc);
 
 export default router;
