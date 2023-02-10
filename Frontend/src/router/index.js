c9556da3087c778c804384c11d86ffada84f9321
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-10 03:00:45 -0600

    added route for profile

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -92,6 +92,21 @@ const routes = [
       },
     ],
   },
+  {
+    path: "/Profile",
+    component: () => import("@/layouts/default/Default.vue"),
+    children: [
+      {
+        path: "",
+        name: "Profile",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+          import(/* webpackChunkName: "home" */ "@/views/Profile.vue"),
+      },
+    ],
+  },
 ];
 
 const router = createRouter({
