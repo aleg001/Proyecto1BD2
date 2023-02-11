5b9bb0c2197428b7cbb87ee2dccd48dfbbd5e6c3
Ale Gomez <gom20347@uvg.edu.gt>
2023-02-11 00:38:07 -0600

    added search songs view

--- Frontend/src/views/SearchSongs.vue
+++ Frontend/src/views/SearchSongs.vue
@@ -1,1 +1,24 @@
-template
+<template>
+    <AppBar />
+    <h1 class=" mt-15 text-h2 font-weight-bold text-center titleText">Buscar canciones</h1>
+    <div>
+
+    </div>
+</template>
+
+
+<script>
+import AppBar from '@/components/AppBar.vue';
+
+export default {
+    components: {
+        AppBar,
+    },
+};
+</script>
+
+<style>
+.titleText {
+    font-family: 'Radio Canada', sans-serif !important;
+}
+</style>
