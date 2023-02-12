7074135c4b3b0efa25de25cf28b379755820e407
Racso1624 <fer10_lb@outlook.com>
2023-02-12 12:36:26 -0600

    Se cambia schema music

--- Backend/models/Music.js
+++ Backend/models/Music.js
@@ -1,12 +1,9 @@
 import mongoose from 'mongoose'
 
 const MusicSchema = new mongoose.Schema({
-  Username: { type: String, required: true, trim: true },
-  Nombre: { type: String, required: true, trim: true },
-  Apellido: { type: String, required: true, trim: true },
-  Email: { type: String, required: true, trim: true },
-  password: { type: String, required: true, trim: true },
-  Playlist: { type: Object, required: true, trim: true },
+  artist: { type: Object, required: true, trim: true },
+  release: { type: Object, required: true, trim: true },
+  song: { type: Object, required: true, trim: true },
 })
 
 const MusicModel = mongoose.model('music', MusicSchema)
