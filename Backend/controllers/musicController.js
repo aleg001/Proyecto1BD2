f0182370e606b346a931b66dcd639cf7f7fcf3ce
Racso1624 <fer10_lb@outlook.com>
2023-02-13 18:41:37 -0600

    Se crean estadisticas

--- Backend/controllers/musicController.js
+++ Backend/controllers/musicController.js
@@ -20,6 +20,28 @@ class MusicController {
     }
   }
 
+  static findStatistics1 = async (req, res) => {
+    try {
+      const docs = await MusicModel.aggregate([
+        {
+          $group: {
+            _id: "$artist",
+            popularidad: { $sum: "$Popularity- The higher the value the more popular the song is" }
+          }
+        },
+        {
+          $sort: { popularidad: -1 }
+        },
+        {
+          $limit: 10
+        }
+      ])
+      res.json(docs)
+    } catch (err) {
+      res.status(500).json({ message: err.message })
+    }
+  }
+
 }
 
 export default MusicController
